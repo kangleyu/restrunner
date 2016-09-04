9a66b0a19717acef9d0b7bdde03f162fd7cfacbc
Tom Yu <kangleyu@gmail.com>
2016-09-04 16:55:32 +0800

    add function for allowing testing GET request

--- static/js/controllers/main_controller.js
+++ static/js/controllers/main_controller.js
@@ -1,7 +1,15 @@
 angular.module('main.module', [])
-  .controller('restrunnerCtrl', ['$scope', function($scope) {
-    // controller content
-  }])
-  .controller('restHttpMethodCtrl', ['$scope', function($scope) {
-    // controller content
+  .controller('restrunnerCtrl', ['$scope', '$http', function($scope, $http) {
+    // Initialization for all controls
+    $scope.httpMethods = ["GET", "POST", "PUT", "DELETE", "PATCH"];
+    $scope.selectedMethod = "GET";
+    $scope.urlAddress = "";
+    $scope.rawResponse = "";
+
+    $scope.execute = function() {
+      $http.get($scope.urlAddress).
+        success(function(data) {
+            $scope.rawResponse = data;
+        });
+    };
   }]);
--- static/js/directives/resthttpmethod.js
+++
@@ -1,6 +0,0 @@
-angular.module('main.module')
-  .directive('restHttpMethod', function () {
-    return {
-      templateUrl: 'js/directives/templates/resthttpmethod.html'
-    };
-  });
